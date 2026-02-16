10d1789ac65af8686ddf36b1d1ee88a4d4ed5f4f
Nico Flores <itsme.nicoflores@gmail.com>
2026-02-16 15:43:18 +0800

    `Added new endpoint to retrieve employee birthdays and updated related code`

--- app/Http/Resources/UserLoginResource.php
+++ app/Http/Resources/UserLoginResource.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserLoginResource extends JsonResource
 {
-    public function toArray($request)
+    public function toArray(Request $request)
     {
         $employee = $this->employee;
 
